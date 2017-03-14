48822088eb469d97fe03101b88872273d4f9fc1d
Steve Olsen <stevelolsen@gmail.com>
2017-03-13 17:34:38 -0700

    Create User table

---
+++ db/migrate/20170314002801_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, [:username, :email], unique: true
+  end
+end
