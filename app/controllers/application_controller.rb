7a52e0e01f6ad593d2e2fbe05f88b5578e85c8df
Steve Olsen <stevelolsen@gmail.com>
2017-03-13 17:51:06 -0700

    Setup Application, Sessions, and User Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :signed_in?
+
+
+  private
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_token!
+  end
+
+  def sign_out
+    @current_user.reset_token!
+    session[:session_token] = nil
+  end
+
+  def require_signed_in!
+    redirect_to new_session_url unless signed_in?
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
 end
