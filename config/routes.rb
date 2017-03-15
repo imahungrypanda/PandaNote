64365d773831f88c1b1d96982000620418d51265
Steve Olsen <stevelolsen@gmail.com>
2017-03-15 16:33:50 -0700

    React minification fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
-  
+
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+
+    resources :notes, only: [:create, :show, :index, :update, :destroy]
   end
 end
---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/panda_note.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
