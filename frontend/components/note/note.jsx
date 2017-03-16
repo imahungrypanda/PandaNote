68b34596cccd2c166d55fd1b7681aaeed116c7b4
Steve Olsen <stevelolsen@gmail.com>
2017-03-16 11:48:34 -0700

    Sidebar styling has icons and are in a column

--- frontend/components/note/note.jsx
+++ frontend/components/note/note.jsx
@@ -17,26 +17,29 @@ class Note extends React.Component {
   render () {
     return (
       <div className="note-container">
-        <h1>Note container</h1>
+        
+        <aside className="sidebar">
+          <section className="sidebar-logo">
+            <img className="logo" src="http://res.cloudinary.com/dbf0xwan5/image/upload/v1489603019/panda_logo_fhv0z7.png" alt="logo"></img>
+          </section>
 
-<aside className="sidebar">
-  <section className="sidebar-logo">
-    <img className="logo" src="http://res.cloudinary.com/dbf0xwan5/image/upload/v1489603019/panda_logo_fhv0z7.png" alt="logo"></img>
-  </section>
+          <section className="sidebar-new-note">
+            <img className="new-note-icon" src="http://res.cloudinary.com/dbf0xwan5/image/upload/q_10/v1489684767/plus_yfk9un.png" alt="new-note"></img>
+          </section>
 
-  <section className="sidebar-new-note">
+          <section className="sidebar-actions">
+            <img className="notes-icon" src="http://res.cloudinary.com/dbf0xwan5/image/upload/q_10/v1489684767/note_lh9a6u.png" alt="notes"></img>
+            <img className="notebook-icon" src="http://res.cloudinary.com/dbf0xwan5/image/upload/q_10/v1489684767/notebook_jjgcyh.png" alt="notebooks"></img>
+            <img className="tags-icon" src="http://res.cloudinary.com/dbf0xwan5/image/upload/q_10/v1489684767/price-tag_ddv6gg.png" alt="tags"></img>
+          </section>
 
-  </section>
+          <section className="user-profile">
+            <img className="user-profile-icon" src="http://res.cloudinary.com/dbf0xwan5/image/upload/q_10/v1489684767/user_v22kdp.png" alt="profile"></img>
+          </section>
 
-  <section className="sidebar-actions">
-    
-  </section>
+        </aside>
 
-  <section className="user-profile"></section>
-
-</aside>
-
-<section className="notes-index"></section>
+        <section className="notes-index"></section>
 
 
     <input className="" type="button" value="Logout" onClick={this.noteLogout} />
