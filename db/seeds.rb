7bd14ec6e20e67fab6652d9eccefeac8b61af3b1
Steve Olsen <stevelolsen@gmail.com>
2017-03-18 22:14:00 -0700

    Update seed so that heroku db guest will have data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,5 @@ User.create(username: "guest", password: "password")
 text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 25.times do |i|
-  Note.create(title: "test#{i}", body: text, user_id: 6, notebook_id: 0)
-end
-25.times do |i|
   Note.create(title: "test#{i}", body: text, user_id: 1, notebook_id: 0)
 end
