ef5ed3ae711155777e8773e6cb2cb8f27262365d
Steve Olsen <stevelolsen@gmail.com>
2017-03-22 21:00:09 -0700

    Create tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,4 +9,7 @@
 #
 
 class Tag < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :taggings
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -10,4 +10,8 @@
 #
 
 class Tagging < ActiveRecord::Base
+  validates :note_id, :tag_id, presence: true
+
+  belongs_to :note
+  belongs_to :tag
 end
