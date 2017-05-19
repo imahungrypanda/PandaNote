cdf2d80d8d1ada36a3903c3e1a41bf1bb9068b93
Steve Olsen <stevelolsen@gmail.com>
2017-05-18 17:24:39 -0700

    Update splash screen

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,9 +14,16 @@ const App = ({ children }) => (
       <Greeting />
     </header>
     <section className="splash-photo">
-      <h1 className="splash-tagline">Remember everything.</h1>
+      <h1 className="splash-tagline">A better way to take notes.</h1>
       </section>
-    <footer className="splash-footer"></footer>
+    <footer className="splash-footer">
+      <a className="github" target="_blank" href="https://github.com/imahungrypanda/Tetris">
+        <img className="github-img" src="https://res.cloudinary.com/dbf0xwan5/image/upload/v1495152995/GitHub-Mark_copy_p9tnvj.png" alt="" />
+      </a>
+      <a className="linkedin" target="_blank" href="https://www.linkedin.com/in/stevelolsen/">
+        <img className="linkedin-img" src="https://res.cloudinary.com/dbf0xwan5/image/upload/v1495152995/linkedin_circle_color-512_copy_mgrxdu.png" alt="" />
+      </a>
+    </footer>
     { children }
   </div>
 );
