e1765f36ea7067f0b1ec3101a23dc64792c2e640
Steve Olsen <stevelolsen@gmail.com>
2017-08-01 14:54:09 -0700

    Note rspec tests

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let! (:note) { create(:note) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:notebook_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:notebook) }
+    it { should have_many(:taggings) }
+    it { should have_many(:tags) }
+  end
 end
