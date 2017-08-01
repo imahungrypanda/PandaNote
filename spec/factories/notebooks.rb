4579cd734055442280922038583f0a7fbfd56084
Steve Olsen <stevelolsen@gmail.com>
2017-08-01 15:11:49 -0700

    Notebook rspec tests

--- spec/factories/notebooks.rb
+++ spec/factories/notebooks.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :notebook do
-    
+    user_id 1
+    title 'test notebook'
   end
 end
--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Notebook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notebook) { create(:notebook) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:title) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:notes) }
+  end
 end
